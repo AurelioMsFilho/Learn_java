f56571a24bae164dab1524d9a60e4b6b64e0c188
AurelioMsFilho <Aureliofilhotj@gmail.com>
2022-08-21 21:37:09 -0300

    Área do Circulo

--- Programs Java/src/Fundamentos_java/AreaCirculo.java
+++ Programs Java/src/Fundamentos_java/AreaCirculo.java
@@ -2,9 +2,12 @@ package Fundamentos_java;
 
 public class AreaCirculo {
 	public static void main(String[] args) {
-		double rad;
+		double rad = 10;
 		final double PI = 3.14159;
-		rad = 10;
+		double area = PI*rad*rad;
+		
+		System.out.println("A área de circulo é: "+ area);
+		
 		
 		
 	}
