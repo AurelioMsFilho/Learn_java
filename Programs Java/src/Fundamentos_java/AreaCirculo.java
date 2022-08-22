3220f39d6fcba0728fb166a6bb4bafbcfa098d92
AurelioMsFilho <Aureliofilhotj@gmail.com>
2022-08-21 21:33:06 -0300

    Fundamentos em Java

---
+++ Programs Java/src/Fundamentos_java/AreaCirculo.java
@@ -0,0 +1,12 @@
+package Fundamentos_java;
+
+public class AreaCirculo {
+	public static void main(String[] args) {
+		double rad;
+		final double PI = 3.14159;
+		rad = 10;
+		
+		
+	}
+
+}
--- Programs Java/src/Fundamentos_java/Exercicio01.java
+++ Programs Java/src/Fundamentos_java/Exercicio01.java
@@ -1,6 +1,6 @@
 package Fundamentos_java;
 
-public class Aula01 {
+public class Exercicio01 {
 	public static void main(String[] args) {
 		System.out.println("Aurélio Filho");
 	}
