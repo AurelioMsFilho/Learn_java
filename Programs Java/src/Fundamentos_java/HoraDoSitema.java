96123c9e3de33c32af0f5b579935f37227d48a54
AurelioMsFilho <Aureliofilhotj@gmail.com>
2022-09-01 09:21:51 -0300

    Hora do sistema

---
+++ Programs Java/src/Fundamentos_java/HoraDoSitema.java
@@ -0,0 +1,14 @@
+package Fundamentos_java;
+
+import java.util.Date;
+
+public class HoraDoSitema {
+	public static void main(String[] args) {
+		
+	Date relogio = new Date();
+	
+	System.out.println("Hora de Brasilia: " + relogio.toString());
+	
+
+}
+}
